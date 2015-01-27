0f0c8e08cfb7e6fb62cb8d5a006012ee953f11cb
Aaron Petcoff <hello@aaronpetcoff.me>
2015-01-27 10:38:59 -0500

    adds navigation

--- js/main.js
+++ js/main.js
@@ -1,35 +1,100 @@
-$(document).ready(function() {
-  $(".big").height($(window).outerHeight());
-  $("video")[0].play();
-});
+(function($) {
+
+  /* Autohider by Aaron Petcoff https://gist.github.com/aptkf/1a5ffdad876a80610b63 */
+ 
+  $.fn.autohider = function(args) {
+ 
+    defaults = {
+      toggle: "visible",
+      buffer:70,
+      tolerance: 8
+    };
+ 
+    args = $.extend(defaults, args);
+ 
+    return this.each(function() {
+      
+      var last;
+      var $this = $(this);
+ 
+      $(window).scroll(function() {
+ 
+        var current = $(this).scrollTop();
+ 
+        if(current > last && current >= args.buffer) {
+          // scrolling down and past the buffer
+          $this.removeClass(args.toggle);
+        } else if (last - current > args.tolerance && !(current < args.buffer)) {
+          // scrolling up past the tolerance level
+          $this.addClass(args.toggle);
+        } else if (current < args.buffer && $this.hasClass(args.toggle)) {
+          $this.removeClass(args.toggle);
+        }
+
+        // record last scroll position
+        last = current;
+      });
+    });
+  };
+})(jQuery);
+
 
 $(document).ready(function() {
   "use strict";
 
-  var formData = {};
+  $(".big").height($(window).outerHeight());
+  $("main").css("marginTop",$(window).outerHeight());
 
-  $("#form").on("submit", function(e) {
-    e.preventDefault();
+  (function() {
+    var header = $('header').position().top+$('header').outerHeight(true);
+    var $navTop = $(".nav-top");
 
-    $("button").prop("disabled",true);
-
-    formData.name = $(".contact-name").val();
-    formData.tel = $(".contact-phone").val();
-    formData.email = $(".contact-email").val();
-    formData.message = $(".contact-message").val();
-
-    $.ajax({
-      dataType:"jsonp",
-      url:"http://getsimpleform.com/messages/ajax?form_api_token=d9661666346512e9947ea6fca1bcddf8",
-      data: formData
-    }).done(function() {
-      $("#contact").addClass("success");
-      $(".contact-confirm").text("Thank you for contacting us.");
-      $(".contact-name,.contact-phone,.contact-email,.contact-message").val(undefined);
-    }).error(function() {
-      $("#contact").addClass("error");
-      $(".contact-confirm").text("There was an error sending your message.");
-      $("button").prop("disabled",false);
+    $(".header-link").click(function(e) {
+      e.preventDefault();
+      $("html, body").animate({
+        scrollTop:header
+      }, 1000);
     });
-  });
+
+    $navTop.click(function(e) {
+      e.preventDefault();
+      $("html, body").animate({
+        scrollTop:0
+      }, 1000);
+    });
+
+    $navTop.autohider({
+      buffer:header
+    })
+  })();
+
+  (function() {
+
+    var formData = {};
+
+    $("#form").on("submit", function(e) {
+      e.preventDefault();
+
+      $("button").prop("disabled",true);
+
+      formData.name = $(".contact-name").val();
+      formData.tel = $(".contact-phone").val();
+      formData.email = $(".contact-email").val();
+      formData.message = $(".contact-message").val();
+
+      $.ajax({
+        dataType:"jsonp",
+        url:"http://getsimpleform.com/messages/ajax?form_api_token=d9661666346512e9947ea6fca1bcddf8",
+        data: formData
+      }).done(function() {
+        $("#contact").addClass("success");
+        $(".contact-confirm").text("Thank you for contacting us.");
+        $(".contact-name,.contact-phone,.contact-email,.contact-message").val(undefined);
+      }).error(function() {
+        $("#contact").addClass("error");
+        $(".contact-confirm").text("There was an error sending your message.");
+        $("button").prop("disabled",false);
+      });
+    });
+  })();
 });
