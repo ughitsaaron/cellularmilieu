c4217bd769cfa740edfa417d7c5b904264d3ac6f
Aaron Petcoff <hello@aaronpetcoff.me>
2015-02-01 21:42:54 -0500

    adds link to youtube video

--- js/main.js
+++ js/main.js
@@ -1,3 +1,52 @@
+if(Modernizr.cssvhunit) {
+  $(".big").height($(window).outerHeight());
+  $("main").css("marginTop",$(window).outerHeight());
+}
+
+// Fires whenever a player has finished loading
+function onPlayerReady(event) { event.target.playVideo(); }
+
+// Fires when the player's state changes.
+function onPlayerStateChange(event) {
+    // Go to the next video after the current one is finished playing
+    if (event.data === 0) {
+      $.fancybox.next();
+    }
+  }
+
+// The API will call this function when the page has finished downloading the JavaScript for the player API
+function onYouTubePlayerAPIReady() {
+
+  // Initialise the fancyBox after the DOM is loaded
+  $(".fancybox")
+  .attr('rel', 'gallery')
+  .fancybox({
+    openEffect  : 'none',
+    closeEffect : 'none',
+    nextEffect  : 'none',
+    prevEffect  : 'none',
+    padding     : 0,
+    margin      : 50,
+    helpers     : {
+      overlay: {
+        locked: false
+      }
+    },
+    beforeShow  : function() {
+      // Find the iframe ID
+      var id = $.fancybox.inner.find('iframe').attr('id');
+      
+      // Create video player object and add event listeners
+      var player = new YT.Player(id, {
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }
+      });
+    }
+  });
+}
+
 (function($) {
   "use strict";
 
